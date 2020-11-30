3206d87f39a3664b405fcb5efaa5be38c027d7e2
hoshizima <ko.0027.oshim@gmail.com>
2020-12-01 01:23:13 +0900

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,103 @@
+/**
+ * 
+ * @param {canvasElement} ctx 
+ * @param {Array} points 
+ * @param {boolean} isClosed 
+ */
+function myDrawContour(
+    ctx,
+    points,
+    isClosed = false
+)
+{
+    ctx.beginPath()
+
+    points.slice(1).forEach(({ x, y }, prevIdx) =>
+    {
+        const from = points[prevIdx]
+        ctx.moveTo(from.x, from.y)
+        ctx.lineTo(x, y)
+    })
+
+    if (isClosed)
+    {
+        const from = points[points.length - 1]
+        const to = points[0]
+        if (!from || !to)
+        {
+            return
+        }
+
+        ctx.moveTo(from.x, from.y)
+        ctx.lineTo(to.x, to.y)
+    }
+
+    ctx.stroke()
+}
+
+/**
+ * 
+ */
+class MyFaceLandmarks
+{
+    constructor(positions)
+    {
+        this.positions = positions;
+    }
+    getJawOutline()
+    {
+        return this.positions.slice(0, 17)
+    }
+
+    getLeftEyeBrow()
+    {
+        return this.positions.slice(17, 22)
+    }
+
+    getRightEyeBrow()
+    {
+        return this.positions.slice(22, 27)
+    }
+
+    getNose()
+    {
+        return this.positions.slice(27, 36)
+    }
+
+    getLeftEye()
+    {
+        return this.positions.slice(36, 42)
+    }
+
+    getRightEye()
+    {
+        return this.positions.slice(42, 48)
+    }
+
+    getMouth()
+    {
+        return this.positions.slice(48, 68)
+    }
+}
+
+/**
+ * 
+ */
+class MyDrawFaceLandmarks
+{
+    constructor(faceLandmarks)
+    {
+        this.faceLandmarks = faceLandmarks
+    }
+
+    draw(ctx)
+    {
+        drawContour(ctx, this.faceLandmarks.getJawOutline())
+        drawContour(ctx, this.faceLandmarks.getLeftEyeBrow())
+        drawContour(ctx, this.faceLandmarks.getRightEyeBrow())
+        drawContour(ctx, this.faceLandmarks.getNose())
+        drawContour(ctx, this.faceLandmarks.getLeftEye(), true)
+        drawContour(ctx, this.faceLandmarks.getRightEye(), true)
+        drawContour(ctx, this.faceLandmarks.getMouth(), true)
+    }
+}
