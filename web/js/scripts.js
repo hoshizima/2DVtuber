7354b581a42d4b9ddfb2b31960bf3780ca511634
hoshizima <ko.0027.oshim@gmail.com>
2021-07-26 22:34:41 +0900

    jsを分けた

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,5 +1,58 @@
+//各要素を取得
+const loader = document.getElementById('loader');
+const videosizeslider = document.getElementById('videosizeslider');
+const maincontents = document.getElementById('maincontents');
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const ctx2d = canvas.getContext('2d');
+const relativepositionX = document.getElementById('relativepositionX');
+const relativepositionY = document.getElementById('relativepositionY');
+//
+const img = new Image();
+//
+let
 /**
- * 
+ * ローディング表示を除去します。
+ */
+function removeLoadingScene()                                                                                                                                                                                                                                                                                                                                                                                                                                               
+{
+    //ビデオ要素をCanvasの後ろへ移動
+    video.style.zIndex = "0";
+    //高さ調整
+    maincontents.style.height = canvas.height + "px";
+    //ローディング画面を除去
+    loader.remove();
+}
+/**
+ * カメラからの入力映像を隠します。
+ */
+function displayChange()
+{
+    if (document.settings.hidevideo.checked)
+    {
+        canvas.style.backgroundColor = "rgba(255,255,2550,0)";
+        videosizeslider.setAttribute("disable", true);
+
+    }
+    else
+    {
+        canvas.style.backgroundColor = "rgba(255,255,2550,1)";
+    }
+}
+/**
+ * カメラ映像のサイズを調整します。
+ */
+function videoSizeChange()
+{
+    video.style.width = videosizeslider.value + "%";
+}
+
+function lockimgposition()
+{
+    
+}
+/**
+ * 座標とキャンバスコンテキストを受け取り線を描画する。
  * @param {canvasElement} ctx 
  * @param {Array} points 
  * @param {boolean} isClosed 
@@ -10,33 +63,33 @@ function myDrawContour(
     isClosed = false
 )
 {
-    ctx.beginPath()
+    ctx.beginPath();
 
     points.slice(1).forEach(({ x, y }, prevIdx) =>
     {
-        const from = points[prevIdx]
-        ctx.moveTo(from.x, from.y)
-        ctx.lineTo(x, y)
+        const from = points[prevIdx];
+        ctx.moveTo(from._x, from._y);
+        ctx.lineTo(x, y);
     })
 
     if (isClosed)
     {
-        const from = points[points.length - 1]
-        const to = points[0]
+        const from = points[points.length - 1];
+        const to = points[0];
         if (!from || !to)
         {
-            return
+            return;
         }
 
-        ctx.moveTo(from.x, from.y)
-        ctx.lineTo(to.x, to.y)
+        ctx.moveTo(from._x, from._y);
+        ctx.lineTo(to._x, to._y);
     }
 
-    ctx.stroke()
+    ctx.stroke();
 }
 
 /**
- * 
+ * 顔パーツの座標を切り取る際に利用する。
  */
 class MyFaceLandmarks
 {
@@ -46,59 +99,97 @@ class MyFaceLandmarks
     }
     getJawOutline()
     {
-        return this.positions.slice(0, 17)
+        return this.positions.slice(0, 17);
     }
 
     getLeftEyeBrow()
     {
-        return this.positions.slice(17, 22)
+        return this.positions.slice(17, 22);
     }
 
     getRightEyeBrow()
     {
-        return this.positions.slice(22, 27)
+        return this.positions.slice(22, 27);
     }
 
     getNose()
     {
-        return this.positions.slice(27, 36)
+        return this.positions.slice(27, 36);
     }
 
     getLeftEye()
     {
-        return this.positions.slice(36, 42)
+        return this.positions.slice(36, 42);
     }
 
     getRightEye()
     {
-        return this.positions.slice(42, 48)
+        return this.positions.slice(42, 48);
     }
 
     getMouth()
     {
-        return this.positions.slice(48, 68)
+        return this.positions.slice(48, 68);
     }
 }
 
 /**
+ * キャンバスへ顔を描画する。
  * 
  */
 class MyDrawFaceLandmarks
 {
     constructor(faceLandmarks)
     {
-        this.faceLandmarks = faceLandmarks
+        this.faceLandmarks = new MyFaceLandmarks(faceLandmarks);
     }
 
     draw(ctx)
     {
-        
-        myDrawContour(ctx, this.faceLandmarks.getJawOutline())
-        myDrawContour(ctx, this.faceLandmarks.getLeftEyeBrow())
-        myDrawContour(ctx, this.faceLandmarks.getRightEyeBrow())
-        myDrawContour(ctx, this.faceLandmarks.getNose())
-        myDrawContour(ctx, this.faceLandmarks.getLeftEye(), true)
-        myDrawContour(ctx, this.faceLandmarks.getRightEye(), true)
-        myDrawContour(ctx, this.faceLandmarks.getMouth(), true)
+        //myDrawContour(ctx, this.faceLandmarks.getJawOutline());
+        myDrawContour(ctx, this.faceLandmarks.getLeftEyeBrow());
+        myDrawContour(ctx, this.faceLandmarks.getRightEyeBrow());
+        myDrawContour(ctx, this.faceLandmarks.getNose());
+        myDrawContour(ctx, this.faceLandmarks.getLeftEye(), true);
+        myDrawContour(ctx, this.faceLandmarks.getRightEye(), true);
+        myDrawContour(ctx, this.faceLandmarks.getMouth(), true);
     }
 }
+
+
+async function onPlay()
+{
+    img.src = 'img/unchi.png';
+    const result = await faceapi.detectSingleFace(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks();
+
+    if (result)
+    {
+        const dims = faceapi.matchDimensions(canvas, video, true);
+        const resizedResults = faceapi.resizeResults(result, dims);
+        const resizedResultsBox = resizedResults['alignedRect']['_box'];
+        removeLoadingScene();
+        //canvas初期化
+        ctx2d.fillStyle = 'rgb(255,255,255)';
+        ctx2d.fillRect(0, 0, ctx2d.width, ctx2d.height);
+        //画像を描画
+        ctx2d.drawImage(img, resizedResultsBox['x'], resizedResultsBox['y'], resizedResultsBox['width'], resizedResultsBox['height']);
+        await new MyDrawFaceLandmarks(resizedResults['landmarks']['_positions']).draw(ctx2d);
+    }
+    setTimeout(() => onPlay());
+}
+
+async function run()
+{
+    // load face detection and face landmark models
+    await faceapi.nets.tinyFaceDetector.load("models/");
+    await faceapi.nets.faceLandmark68Net.load("models/");
+    // try to access users webcam and stream the images
+    // to the video element
+    const stream = await navigator.mediaDevices.getUserMedia({ video: {} });
+    video.srcObject = stream;
+}
+
+document.addEventListener('DOMContentLoaded', function ()
+{
+    run();
+})
